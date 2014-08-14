b938948c85809f9ff3145e10f68beef44513faf7
Francois Paul <francois@rails.co.za>
2014-08-14 15:16:18 +0200

    float instead of bd

--- lib/private_api.rb
+++ lib/private_api.rb
@@ -40,8 +40,8 @@ module PrivateApi
     params = {
       pair: pair,
       type: order_type,
-      volume: volume.to_d.round(6).to_s,
-      price: price.to_d.round(6).to_s
+      volume: volume.to_f.round(6).to_s,
+      price: price.to_f.round(6).to_s
     }
     opt.merge!({params: params, method: :post})
     path = '/api/1/postorder'
